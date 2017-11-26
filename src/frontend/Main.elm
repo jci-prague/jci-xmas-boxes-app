4c2ad477f613f6757636fca14d19c07e67405ed2
Tom Metz <tomas@metzovi.net>
2017-11-26 10:31:43 +0100

    Buttons for setting bottom and top Age tresholds

--- src/frontend/Main.elm
+++ src/frontend/Main.elm
@@ -25,15 +25,70 @@ type alias Family =
 
 type alias Model =
     { families : List Family
+    , bottomThreshold: Int
+    , topThreshold: Int
     }
 
 
 type Msg
-    = None
+    = SetBottomThreshold Int
+    | SetTopThreshold Int
+    | None
 
 
 view : Model -> Html Msg
 view model =
+  div []
+      [ (filterFormView model)
+      , (viewFamilies model)
+      ]
+
+
+filterFormView : Model -> Html Msg
+filterFormView model =
+  div [] 
+      [ span [] [text "Věk od"]
+      , button [ onClick (SetBottomThreshold  1) ] [ text  "1" ]
+      , button [ onClick (SetBottomThreshold  2) ] [ text  "2" ]
+      , button [ onClick (SetBottomThreshold  3) ] [ text  "3" ]
+      , button [ onClick (SetBottomThreshold  4) ] [ text  "4" ]
+      , button [ onClick (SetBottomThreshold  5) ] [ text  "5" ]
+      , button [ onClick (SetBottomThreshold  6) ] [ text  "6" ]
+      , button [ onClick (SetBottomThreshold  7) ] [ text  "7" ]
+      , button [ onClick (SetBottomThreshold  8) ] [ text  "8" ]
+      , button [ onClick (SetBottomThreshold  9) ] [ text  "9" ]
+      , button [ onClick (SetBottomThreshold 10) ] [ text "10" ]
+      , button [ onClick (SetBottomThreshold 11) ] [ text "11" ]
+      , button [ onClick (SetBottomThreshold 12) ] [ text "12" ]
+      , button [ onClick (SetBottomThreshold 13) ] [ text "13" ]
+      , button [ onClick (SetBottomThreshold 14) ] [ text "14" ]
+      , button [ onClick (SetBottomThreshold 15) ] [ text "15" ]
+      , button [ onClick (SetBottomThreshold 16) ] [ text "16" ]
+      , span []
+        [ (text ((toString model.bottomThreshold) ++ " - " ++ (toString model.topThreshold)))
+        ]
+      , span [] [text "Věk do"]
+      , button [ onClick (SetTopThreshold  2) ] [ text  "2" ]
+      , button [ onClick (SetTopThreshold  3) ] [ text  "3" ]
+      , button [ onClick (SetTopThreshold  4) ] [ text  "4" ]
+      , button [ onClick (SetTopThreshold  5) ] [ text  "5" ]
+      , button [ onClick (SetTopThreshold  6) ] [ text  "6" ]
+      , button [ onClick (SetTopThreshold  7) ] [ text  "7" ]
+      , button [ onClick (SetTopThreshold  8) ] [ text  "8" ]
+      , button [ onClick (SetTopThreshold  9) ] [ text  "9" ]
+      , button [ onClick (SetTopThreshold 10) ] [ text "10" ]
+      , button [ onClick (SetTopThreshold 11) ] [ text "11" ]
+      , button [ onClick (SetTopThreshold 12) ] [ text "12" ]
+      , button [ onClick (SetTopThreshold 13) ] [ text "13" ]
+      , button [ onClick (SetTopThreshold 14) ] [ text "14" ]
+      , button [ onClick (SetTopThreshold 15) ] [ text "15" ]
+      , button [ onClick (SetTopThreshold 16) ] [ text "16" ]
+      , button [ onClick (SetTopThreshold 17) ] [ text "17" ]
+      ]
+
+
+viewFamilies : Model -> Html Msg
+viewFamilies model =
     div []
         [ ul []
             (List.map
@@ -51,9 +106,21 @@ viewFamily : Family -> Html Msg
 viewFamily family =
     div []
         (List.map
-            (\ch -> div [] [ (text ch.name) ])
+            (\ch -> div [ class (classForGender ch) ]
+                [ span [class "childName"] [(text ch.name)]
+                , span [class "childAge"] [(text (toString ch.age))]
+                ]
+            )
             family.children
         )
+
+
+classForGender : Child -> String
+classForGender child =
+    if child.gender == Male then
+        "male"
+    else 
+        "female"
 
 
 initialModel : Model
@@ -62,13 +129,20 @@ initialModel =
         [ familyJimm
         , familyEmaJohny
         , familyMary
+        , familyJessie
         ]
+    , bottomThreshold = 1
+    , topThreshold = 17
     }
 
 
 update : Msg -> (Model -> Model)
 update msg model =
     case msg of
+        SetBottomThreshold threshold ->
+            { model | bottomThreshold = threshold }
+        SetTopThreshold threshold ->
+            { model | topThreshold = threshold }
         None ->
             model
 
@@ -116,5 +190,12 @@ familyMary : Family
 familyMary =
     { children =
         [ { name = "Mary", age = 8, gender = Female }
+        ]
+    }
+
+familyJessie : Family
+familyJessie =
+    { children =
+        [ { name = "Jessie", age = 11, gender = Female }
         ]
     }
